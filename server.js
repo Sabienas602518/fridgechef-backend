fc2aaf90a847d86494e1a64afa5ac476a2414732
Sabiena Jeyaragawan <Sabiena.Jeyaragawan@Student.HTW-Berlin.de>
2026-08-13 01:00:09 +0200

    Backend mit MongoDB verbinden

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
@@ -10,10 +11,27 @@ app.use(express.json());
 app.use(cors());
 app.use('/api', routes);
 
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    { dbName: process.env.DATABASE }
+);
+
+const db = mongoose.connection;
+
+db.on('error', err => {
+    console.log(err);
+});
+
+db.once('open', () => {
+    console.log('connected to DB');
+});
+
 app.listen(PORT, (error) => {
     if (error) {
         console.log(error);
     } else {
-        console.log(`Server started and listening on port ${PORT} ...`);
+        console.log(
+            `Server started and listening on port ${PORT} ...`
+        );
     }
 });
